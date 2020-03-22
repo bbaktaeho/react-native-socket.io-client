efd302914e47bf123c769e8a0daa037c2027cfe7
bbaktaeho <lth9036@gmail.com>
2020-03-22 15:12:58 +0900

    [add/npm]socket.io-clinet

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <View>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+          <Text>ggdgdgdgg</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
